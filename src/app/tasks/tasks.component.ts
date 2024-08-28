e8bba1a45f912c1200b66bc4b315be605b60f380
brunascauri <brunascauri@hotmail.com>
2024-08-28 16:31:41 -0300

    feat: updating files with dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, Output } from '@angular/core';
+
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service'
 
 @Component({
   selector: 'app-tasks',
@@ -13,58 +15,19 @@ import { type NewTaskData } from './task/task.model';
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!:string;
-  // @Output() isAddingTask:boolean = false;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master angular',
-      summary: 'Learn all the basics and advanced angular features',
-      dueDate: '2525-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u2',
-      title: 'Master javascript',
-      summary: 'Learn all the basics and advanced js features',
-      dueDate: '2525-12-31'
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Master c#',
-      summary: 'Learn all the basics and advanced c# features',
-      dueDate: '2525-12-31'
-    },
-  ];
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId == this.userId)
-  }
-
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task => task.id !== id))
+    return this.tasksService.getUserTasks(this.userId)
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
-    this.isAddingTask = false;
-  }
-
 }
-''
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+import { NewTaskData } from "./task/task.model";
+
+@Injectable({providedIn: 'root'})
+
+export class TasksService {
+
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master angular',
+      summary: 'Learn all the basics and advanced angular features',
+      dueDate: '2525-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master javascript',
+      summary: 'Learn all the basics and advanced js features',
+      dueDate: '2525-12-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master c#',
+      summary: 'Learn all the basics and advanced c# features',
+      dueDate: '2525-12-31'
+    },
+  ];
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId == userId)
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task => task.id !== id))
+  }
+}
