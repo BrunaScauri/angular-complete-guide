4de8e25a25dd2a960ffdc4bae0eaeed5761ca297
brunascauri <brunascauri@hotmail.com>
2024-08-29 14:47:39 -0300

    WIP: creating/bootstrapping apps with angular modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TasksComponent } from './tasks/tasks.component'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  // standalone: true,
   imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
