12fdd6fc5abb7af3c5c1e6adeddff5e8cb4114b8
brunascauri <brunascauri@hotmail.com>
2024-08-29 15:37:15 -0300

    feat: Tasks module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SharedModule } from "../shared/shared.module";
+import { FormsModule } from "@angular/forms";
+
+import { TasksComponent } from "./tasks.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { TaskComponent } from "./task/task.component";
 
 @NgModule ({
-  declarations:[]
+  declarations:[TasksComponent, TaskComponent, NewTaskComponent],
+  exports: [TasksComponent], //I can only import the component used by the modules. In this case, only the 'tasks' is used.
+  imports: [SharedModule, CommonModule, FormsModule]
 })
 
 export class TasksModule {}
