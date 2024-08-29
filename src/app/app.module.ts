0fd94bba6103d3b41c45a91886af1611925d4ead
brunascauri <brunascauri@hotmail.com>
2024-08-29 15:09:05 -0300

    feat: card component bootstrapped

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
+import { CardComponent } from "./shared/card/card.component";
 
 @NgModule ({
-  declarations:[AppComponent, HeaderComponent],
+  declarations:[AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, UserComponent, TasksComponent]
+  imports: [BrowserModule, TasksComponent, CardComponent]
 })
 
 export class AppModule {}
